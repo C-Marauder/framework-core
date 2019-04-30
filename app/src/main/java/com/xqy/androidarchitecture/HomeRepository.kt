07f059fd6f9446d056faf4967be239a7feae236f
xqy666666 <17775621462@163.com>
2019-04-30 21:12:43 +0800

    提交代码

--- app/src/main/java/com/xqy/androidarchitecture/HomeRepository.kt
+++ app/src/main/java/com/xqy/androidarchitecture/HomeRepository.kt
@@ -1,11 +1,14 @@
 package com.xqy.androidarchitecture
 
+import androidx.lifecycle.LiveData
 import com.androidx.processor.Service
 import com.androidx.frameworkcore.viewmodel.repository.AppRepository
+import com.androidx.frameworkcore.viewmodel.repository.DataBaseCallback
 import retrofit2.Call
 
 @Service("getThings")
 class HomeRepository: AppRepository<ApiService,String,String>() {
+
     override fun requestFromNetwork(apiService: ApiService, requestType: String): Call<String> {
         return apiService.getThings(requestType)
     }
